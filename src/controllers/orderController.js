0e3561c84b4357f129935264482051ddf86a7cad
Gercius <gerciuswork@gmail.com>
2025-04-08 13:06:38 +0300

    Add restriction to limit orders (one meal per day)

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -25,7 +25,17 @@ const getAllOrders = async (req, res) => {
 };
 
 const addOrder = async (req, res) => {
+    // Get day in 01 format
+    const currentDay = new Date().toLocaleDateString(undefined, {
+        day: "2-digit",
+    });
+
     try {
+        const orderDates = await Order.find({ userId: req.user.id }, { createdAt: 1 });
+
+        const currentDayOrder = orderDates.find((order) => order.createdAt.toISOString().slice(8, 10) == currentDay);
+        if (currentDayOrder) throw new Error("Today's order already exists");
+
         if (!req.body.order) req.body.order = req.params.OrderId;
 
         const newOrder = await Order.create(req.body);
